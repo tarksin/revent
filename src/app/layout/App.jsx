50626f8be7e391168389bd9c91de043e7009e27c
tarksin <tarksin@maxxima.net>
2018-07-06 07:48:15 -0500

    Initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Re-vents 0122</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
